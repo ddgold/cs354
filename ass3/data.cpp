4385d820211b019c100950179e134750d4fb0aa1
Doug <ddg625@cs.utexas.edu>
2013-12-06 16:58:15 -0600

    started ass4

--- ass3/data.cpp
+++ ass3/data.cpp
@@ -19,14 +19,48 @@
 #include "common.h"
 #include "data.h"
 
-GLfloat i0_x[MAX_POINT];    // Iteration 0 control points, x
-GLfloat i0_y[MAX_POINT];    // Iteration 0 control points, y
-int num_i0_pts;                 // The number of iteration 0 control points
+GLfloat originalCP_x[MAX_POINT];
+GLfloat originalCP_y[MAX_POINT];
+int originalCP;
 
-GLfloat *draw_x;     // Control points to be drawn, x
-GLfloat *draw_y;     // Control points to be drawn, y
-int num_draw_pts;    // The number of control points to draw
 
+GLfloat *gouraudCP_x;
+GLfloat *gouraudCP_y;
+GLfloat *gouraudCP_z;
+
+GLfloat *gouraudNorm_x;
+GLfloat *gouraudNorm_y;
+GLfloat *gouraudNorm_z;
+
+int gouraudVerticalCP;
+int gouraudRadialCP;
+
+
+GLfloat *phongCP_x;
+GLfloat *phongCP_y;
+GLfloat *phongCP_z;
+
+GLfloat *phongNorm_x;
+GLfloat *phongNorm_y;
+GLfloat *phongNorm_z;
+
+int phongVerticalCP;
+int phongRadialCP;
+
+
+int verticalSubLevel;
+int radialSubLevel;
+
+
+GLfloat shininess[1] = {55.0f};
+GLfloat specular[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat lightPosition[4] = {1.0f, 1.0f, 1.0f, 0.0f};
+
+
+bool is3D;
+bool isWire;
+bool isPoints;
+bool isPhong;
 
 /**********************************************
  * Begin Routines for Midpoint Subdivsion
@@ -105,25 +139,467 @@ GLfloat *mergePointsArrays(GLfloat *arr1, GLfloat *arr2, int size) {
 	return arr3;
 }
 
-/*
- * Using the iteration 0 control points (which are stored in the global
- * arrays "i0_x" and "i0_y") and the argument subdivision level, sets
- * the globals "GLfloat *draw_x" and "GLfloat *draw_y" to contain the x and
- * y coordinates for all points that need to be drawn at the given
- * subdivision level.  Also sets the number of value of global
- * "num_draw_pts" to be the size of one of these two drawing arrays.
- *
- * Note: The subdivision level should be greater than or equal to 0.  This
- * routine also frees the space used by the old "draw_x" and "draw_y" arrays,
- * if they exist.
- */
-void subdividePointsArray(int subdiv_level) {
+void subdividePointsArray(int subdiv_level) 
+{
 	assert(subdiv_level >= 0);
 
 	/* ADD YOUR CODE HERE */
 
 	return;
 }
+
+void radialSubdivide()
+{
+	++radialSubLevel;
+
+	
+	GLfloat *temp_x = copyPointsArray(gouraudCP_x, gouraudRadialCP * gouraudVerticalCP);
+	GLfloat *temp_y = copyPointsArray(gouraudCP_y, gouraudRadialCP * gouraudVerticalCP);
+	
+	gouraudRadialCP *= 2;
+
+	gouraudCP_x = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+	gouraudCP_y = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+	gouraudCP_z = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+
+	
+
+	int i = 0;
+	int j = 0;
+	float theta = 0;
+	for(j = 0; j < gouraudRadialCP; ++j)
+	{
+		theta = (360.0f / gouraudRadialCP * j) * M_PI / 180.0f; 
+
+		//printf("theta: %f\n",theta);
+
+		for(i = 0; i < gouraudVerticalCP; ++i)
+		{
+			gouraudCP_x[j * gouraudVerticalCP + i] = temp_x[i]*cos(theta);
+			gouraudCP_y[j * gouraudVerticalCP + i] = temp_y[i];
+			gouraudCP_z[j * gouraudVerticalCP + i] = temp_x[i]*sin(theta);
+		}
+	}
+	free(temp_x);
+	free(temp_y);
+
+	generateGouraud();
+	generatePhong();
+}
+
+	void verticalSubdivide()
+	{
+		++verticalSubLevel;
+
+		GLfloat *temp_x = copyPointsArray(gouraudCP_x, gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *temp_y = copyPointsArray(gouraudCP_y, gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *temp_z = copyPointsArray(gouraudCP_z, gouraudRadialCP * gouraudVerticalCP);
+	
+		GLfloat *steady_x = copyPointsArray(gouraudCP_x, gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *steady_y = copyPointsArray(gouraudCP_y, gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *steady_z = copyPointsArray(gouraudCP_z, gouraudRadialCP * gouraudVerticalCP);
+	
+		GLfloat *sub_x = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *sub_y = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+		GLfloat *sub_z = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+
+
+		int i = 0;
+		int j = 0;
+		//int theta = 0;
+		int curSpot = 0;
+		//repeat for all vertical slices
+		for(j = 0; j < gouraudRadialCP; ++j)
+		{
+			//for each vertical point
+			for(i = 0; i < gouraudVerticalCP - 1; ++i)
+			{
+				//make sub_xyz first
+				curSpot = j * gouraudVerticalCP + i;
+				sub_x[curSpot] = (steady_x[curSpot] + steady_x[curSpot+1])/2.0;
+				sub_y[curSpot] = (steady_y[curSpot] + steady_y[curSpot+1])/2.0;
+				sub_z[curSpot] = (steady_z[curSpot] + steady_z[curSpot+1])/2.0;
+
+				if(i > 0)
+				{
+					temp_x[curSpot] = (steady_x[curSpot-1] + 6 * steady_x[curSpot] + steady_x[curSpot+1])/8.0;
+					temp_y[curSpot] = (steady_y[curSpot-1] + 6 * steady_y[curSpot] + steady_y[curSpot+1])/8.0;
+					temp_z[curSpot] = (steady_z[curSpot-1] + 6 * steady_z[curSpot] + steady_z[curSpot+1])/8.0;
+				}
+			}
+		}
+
+	gouraudCP_x = makePointsArray( gouraudRadialCP * (2 * gouraudVerticalCP - 1));
+	gouraudCP_y = makePointsArray( gouraudRadialCP * (2 * gouraudVerticalCP - 1));
+	gouraudCP_z = makePointsArray( gouraudRadialCP * (2 * gouraudVerticalCP - 1));
+
+	int fillPos = 0;
+	for(j = 0; j< gouraudRadialCP; ++j)
+	{
+		for(i = 0; i < gouraudVerticalCP; ++i)
+		{
+			curSpot = j * gouraudVerticalCP + i;
+			gouraudCP_x[fillPos] = temp_x[curSpot];
+			gouraudCP_y[fillPos] = temp_y[curSpot];
+			gouraudCP_z[fillPos] = temp_z[curSpot];
+			++fillPos;
+
+			if(i != gouraudVerticalCP -1)
+			{
+				gouraudCP_x[fillPos] = sub_x[curSpot];
+				gouraudCP_y[fillPos] = sub_y[curSpot];
+				gouraudCP_z[fillPos] = sub_z[curSpot];
+				++fillPos;
+			}
+		}
+	}
+		free(temp_x);
+		free(temp_y);
+		free(temp_z);
+
+		free(steady_x);
+		free(steady_y);
+		free(steady_z);
+
+		free(sub_x);
+		free(sub_y);
+		free(sub_z);
+
+		gouraudVerticalCP = 2 * gouraudVerticalCP - 1;
+
+
+		generateGouraud();
+		generatePhong();
+	}
+
+void add2dControlPoint(int x, int y)
+{
+	if(x <= 205 && x >= 195)
+		originalCP_x[originalCP] = 0;
+	else
+		originalCP_x[originalCP] = (x - 200) /  5.0;
+
+	originalCP_y[originalCP] = (y - 200) / -5.0 ;
+	++originalCP;
+}
+
+void remove2dControlPoint()
+{
+	--originalCP;
+}
+
+void generateRotationPoints(int n)
+{
+	radialSubLevel = 0;
+	verticalSubLevel = 0;
+	gouraudRadialCP = n;
+	gouraudVerticalCP = originalCP + 2;
+
+	gouraudCP_x = (GLfloat *)malloc(gouraudRadialCP * gouraudVerticalCP * sizeof(GLfloat));
+	gouraudCP_y = (GLfloat *)malloc(gouraudRadialCP * gouraudVerticalCP * sizeof(GLfloat));
+	gouraudCP_z = (GLfloat *)malloc(gouraudRadialCP * gouraudVerticalCP * sizeof(GLfloat));
+	
+	int i = 0;
+	int j = 0;
+	float theta = 0;
+	
+	for(j = 0; j < gouraudRadialCP; ++j)
+	{
+		theta = (360.0/gouraudRadialCP * j) * M_PI / 180.0f; 
+
+		gouraudCP_x[j * gouraudVerticalCP] = 0;
+		gouraudCP_y[j * gouraudVerticalCP] = originalCP_y[0];
+		gouraudCP_z[j * gouraudVerticalCP] = 0;
+
+		for(i = 0; i < originalCP; ++i)
+		{
+			gouraudCP_x[j * gouraudVerticalCP + i + 1] = originalCP_x[i] * cos(theta);
+			gouraudCP_y[j * gouraudVerticalCP + i + 1] = originalCP_y[i];
+			gouraudCP_z[j * gouraudVerticalCP + i + 1] = originalCP_x[i] * sin(theta);
+
+		}
+
+		gouraudCP_x[(j + 1) * gouraudVerticalCP - 1] = 0;
+		gouraudCP_y[(j + 1) * gouraudVerticalCP - 1] = originalCP_y[originalCP - 1];
+		gouraudCP_z[(j + 1) * gouraudVerticalCP - 1] = 0;
+
+
+	}
+
+	generateGouraud();
+	generatePhong();
+}
+
+void generateGouraud(void)
+{
+	gouraudNorm_x = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+	gouraudNorm_y = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+	gouraudNorm_z = makePointsArray( gouraudRadialCP * gouraudVerticalCP);
+
+	int i, j;
+	int curSpot;
+	int leftIndex, rightIndex;
+
+	float right[3]      = {0, 0, 0};
+	float top[3]        = {0, 0, 0};
+	float left[3]       = {0, 0, 0};
+	float bottom[3]     = {0, 0, 0};
+	float normal[3]     = {0, 0, 0};
+	float un;
+
+	for(j = 0; j < gouraudRadialCP; ++j)
+	{
+		gouraudNorm_x[j * gouraudVerticalCP] = 0.0f;
+		gouraudNorm_y[j * gouraudVerticalCP] = 1.0f;
+		gouraudNorm_z[j * gouraudVerticalCP] = 0.0f;
+
+
+		//for each vertical point
+		for(i = 1; i < gouraudVerticalCP - 1; ++i)
+		{
+			// Splice
+			curSpot = j * gouraudVerticalCP + i;
+			leftIndex  = curSpot - gouraudVerticalCP;
+			rightIndex = curSpot + gouraudVerticalCP;
+			
+			if (j == 0)
+			{
+				leftIndex = (gouraudRadialCP - 1) * gouraudVerticalCP + i;
+			}
+			
+			if (j == gouraudRadialCP - 1)
+			{
+				rightIndex = i;
+			}
+
+			right[0] = gouraudCP_x[rightIndex] - gouraudCP_x[curSpot];
+			right[1] = gouraudCP_y[rightIndex] - gouraudCP_y[curSpot];
+			right[2] = gouraudCP_z[rightIndex] - gouraudCP_z[curSpot];
+
+			top[0] = gouraudCP_x[curSpot - 1] - gouraudCP_x[curSpot];
+			top[1] = gouraudCP_y[curSpot - 1] - gouraudCP_y[curSpot];
+			top[2] = gouraudCP_z[curSpot - 1] - gouraudCP_z[curSpot];
+
+			left[0] = gouraudCP_x[leftIndex] - gouraudCP_x[curSpot];
+			left[1] = gouraudCP_y[leftIndex] - gouraudCP_y[curSpot];
+			left[2] = gouraudCP_z[leftIndex] - gouraudCP_z[curSpot];
+
+			bottom[0] = gouraudCP_x[curSpot + 1] - gouraudCP_x[curSpot];
+			bottom[1] = gouraudCP_y[curSpot + 1] - gouraudCP_y[curSpot];
+			bottom[2] = gouraudCP_z[curSpot + 1] - gouraudCP_z[curSpot];
+			
+
+
+			//normal
+			normal[0] += (right[1] * top[2] - right[2] * top[1]);
+			normal[1] += (right[2] * top[0] - right[0] * top[2]);
+			normal[2] += (right[0] * top[1] - right[1] * top[0]);
+
+			normal[0] += (top[1] * left[2] - top[2] * left[1]);
+			normal[1] += (top[2] * left[0] - top[0] * left[2]);
+			normal[2] += (top[0] * left[1] - top[1] * left[0]);
+
+			normal[0] += (left[1] * bottom[2] - left[2] * bottom[1]);
+			normal[1] += (left[2] * bottom[0] - left[0] * bottom[2]);
+			normal[2] += (left[0] * bottom[1] - left[1] * bottom[0]);
+
+			normal[0] += (bottom[1] * right[2] - bottom[2] * right[1]);
+			normal[1] += (bottom[2] * right[0] - bottom[0] * right[2]);
+			normal[2] += (bottom[0] * right[1] - bottom[1] * right[0]);
+			
+
+			//normalize
+			un = sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+
+			normal[0] /= (un);
+			normal[1] /= (un);
+			normal[2] /= (un);
+			
+			gouraudNorm_x[curSpot] = normal[0];
+			gouraudNorm_y[curSpot] = normal[1];
+			gouraudNorm_z[curSpot] = normal[2];
+
+			normal[0] = 0;
+			normal[1] = 0;
+			normal[2] = 0;
+		}
+
+		gouraudNorm_x[(j + 1) * gouraudVerticalCP - 1] =  0.0f;
+		gouraudNorm_y[(j + 1) * gouraudVerticalCP - 1] = -1.0f;
+		gouraudNorm_z[(j + 1) * gouraudVerticalCP - 1] =  0.0f;
+	}
+}
+
+void generatePhong(void)
+{
+	phongVerticalCP = 2 * gouraudVerticalCP - 1;
+	phongRadialCP = 2 * gouraudRadialCP;
+
+	phongCP_x =  makePointsArray( phongRadialCP * phongVerticalCP);
+	phongCP_y =  makePointsArray( phongRadialCP * phongVerticalCP);
+	phongCP_z =  makePointsArray( phongRadialCP * phongVerticalCP);
+
+	int i, j;
+  int curSpot;
+  int nextCol;
+
+	//THIS FOR LOOP LOOK THOUGH GOURAUD
+	//DO NO CHANGE gouraudRadialCP or vGCP
+	//NOT GENERATING THE PINTS RIGHT, here lays our problems
+	for(j = 0; j < gouraudRadialCP; ++j)
+	{
+		for(i = 0; i < gouraudVerticalCP ; ++i)
+		{
+			//Vertical points
+			curSpot = j * gouraudVerticalCP + i; 
+			phongCP_x[2 * j * phongVerticalCP + 2 * i ] = gouraudCP_x[curSpot]; 
+			phongCP_y[2 * j * phongVerticalCP + 2 * i ] = gouraudCP_y[curSpot]; 
+			phongCP_z[2 * j * phongVerticalCP + 2 * i ] = gouraudCP_z[curSpot]; 
+
+			if(i != gouraudVerticalCP - 1)
+			{
+				phongCP_x[2 * j * phongVerticalCP + 2 * i  + 1] = (gouraudCP_x[curSpot] + gouraudCP_x[curSpot + 1])/2.0; 
+				phongCP_y[2 * j * phongVerticalCP + 2 * i  + 1] = (gouraudCP_y[curSpot] + gouraudCP_y[curSpot + 1])/2.0; 
+				phongCP_z[2 * j * phongVerticalCP + 2 * i  + 1] = (gouraudCP_z[curSpot] + gouraudCP_z[curSpot + 1])/2.0; 
+			}
+
+			//horrizontal
+			if(j != gouraudRadialCP - 1)
+				nextCol = (j + 1) * gouraudVerticalCP + i;
+			else
+				nextCol = i;
+
+			phongCP_x[(2 * j + 1) * phongVerticalCP + i * 2] = (gouraudCP_x[curSpot] + gouraudCP_x[nextCol])/2.0;
+			phongCP_y[(2 * j + 1) * phongVerticalCP + i * 2] = (gouraudCP_y[curSpot] + gouraudCP_y[nextCol])/2.0;
+			phongCP_z[(2 * j + 1) * phongVerticalCP + i * 2] = (gouraudCP_z[curSpot] + gouraudCP_z[nextCol])/2.0;
+
+			if(i !=gouraudVerticalCP - 1)
+			{
+				phongCP_x[(2 * j + 1) * phongVerticalCP + i * 2 + 1] = (gouraudCP_x[curSpot] + gouraudCP_x[nextCol + 1])/2.0;
+				phongCP_y[(2 * j + 1) * phongVerticalCP + i * 2 + 1] = (gouraudCP_y[curSpot] + gouraudCP_y[nextCol + 1])/2.0;
+				phongCP_z[(2 * j + 1) * phongVerticalCP + i * 2 + 1] = (gouraudCP_z[curSpot] + gouraudCP_z[nextCol + 1])/2.0;
+
+			}
+		}
+	}
+
+	phongNorm_x = makePointsArray( phongRadialCP * phongVerticalCP);
+	phongNorm_y = makePointsArray( phongRadialCP * phongVerticalCP);
+	phongNorm_z = makePointsArray( phongRadialCP * phongVerticalCP);
+
+	
+	
+	int leftIndex, rightIndex;
+
+	float right[3]      = {0, 0, 0};
+	float topRight[3]   = {0, 0, 0};
+	float top[3]        = {0, 0, 0};
+	float left[3]       = {0, 0, 0};
+	float bottomLeft[3] = {0, 0, 0};
+	float bottom[3]     = {0, 0, 0};
+	float normal[3]     = {0, 0, 0};
+	float un;
+
+	for(j = 0; j < phongRadialCP; ++j)
+	{
+		phongNorm_x[j * phongVerticalCP] = 0.0f;
+		phongNorm_y[j * phongVerticalCP] = 1.0f;
+		phongNorm_z[j * phongVerticalCP] = 0.0f;
+
+		//for each vertical point
+		for(i = 1; i < phongVerticalCP - 1; ++i)
+		{
+			// Splice
+			curSpot = j * phongVerticalCP + i;
+			leftIndex  = curSpot - phongVerticalCP;
+			rightIndex = curSpot + phongVerticalCP;
+			
+			if (j == 0)
+			{
+				leftIndex = (phongRadialCP - 1) * phongVerticalCP + i;
+			}
+			
+			if (j == phongRadialCP - 1)
+			{
+				rightIndex = i;
+			}
+
+			right[0] = phongCP_x[rightIndex] - phongCP_x[curSpot];
+			right[1] = phongCP_y[rightIndex] - phongCP_y[curSpot];
+			right[2] = phongCP_z[rightIndex] - phongCP_z[curSpot];
+
+			topRight[0] = phongCP_x[rightIndex - 1] - phongCP_x[curSpot];
+			topRight[1] = phongCP_y[rightIndex - 1] - phongCP_y[curSpot];
+			topRight[2] = phongCP_z[rightIndex - 1] - phongCP_z[curSpot];
+
+			top[0] = phongCP_x[curSpot - 1] - phongCP_x[curSpot];
+			top[1] = phongCP_y[curSpot - 1] - phongCP_y[curSpot];
+			top[2] = phongCP_z[curSpot - 1] - phongCP_z[curSpot];
+
+			left[0] = phongCP_x[leftIndex] - phongCP_x[curSpot];
+			left[1] = phongCP_y[leftIndex] - phongCP_y[curSpot];
+			left[2] = phongCP_z[leftIndex] - phongCP_z[curSpot];
+
+			bottomLeft[0] = phongCP_x[leftIndex + 1] - phongCP_x[curSpot];
+			bottomLeft[1] = phongCP_y[leftIndex + 1] - phongCP_y[curSpot];
+			bottomLeft[2] = phongCP_z[leftIndex + 1] - phongCP_z[curSpot];
+
+			bottom[0] = phongCP_x[curSpot + 1] - phongCP_x[curSpot];
+			bottom[1] = phongCP_y[curSpot + 1] - phongCP_y[curSpot];
+			bottom[2] = phongCP_z[curSpot + 1] - phongCP_z[curSpot];
+			
+
+
+			//normal
+			normal[0] += (right[1] * topRight[2] - right[2] * topRight[1]);
+			normal[1] += (right[2] * topRight[0] - right[0] * topRight[2]);
+			normal[2] += (right[0] * topRight[1] - right[1] * topRight[0]);
+
+			normal[0] += (topRight[1] * top[2] - topRight[2] * top[1]);
+			normal[1] += (topRight[2] * top[0] - topRight[0] * top[2]);
+			normal[2] += (topRight[0] * top[1] - topRight[1] * top[0]);
+
+			normal[0] += (top[1] * left[2] - top[2] * left[1]);
+			normal[1] += (top[2] * left[0] - top[0] * left[2]);
+			normal[2] += (top[0] * left[1] - top[1] * left[0]);
+
+			normal[0] += (left[1] * bottomLeft[2] - left[2] * bottomLeft[1]);
+			normal[1] += (left[2] * bottomLeft[0] - left[0] * bottomLeft[2]);
+			normal[2] += (left[0] * bottomLeft[1] - left[1] * bottomLeft[0]);
+
+			normal[0] += (bottomLeft[1] * bottom[2] - bottomLeft[2] * bottom[1]);
+			normal[1] += (bottomLeft[2] * bottom[0] - bottomLeft[0] * bottom[2]);
+			normal[2] += (bottomLeft[0] * bottom[1] - bottomLeft[1] * bottom[0]);
+
+			normal[0] += (bottom[1] * right[2] - bottom[2] * right[1]);
+			normal[1] += (bottom[2] * right[0] - bottom[0] * right[2]);
+			normal[2] += (bottom[0] * right[1] - bottom[1] * right[0]);
+			
+
+			//normalize - divide first then normalizE? think 
+			un = sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+
+			normal[0] /= (un);
+			normal[1] /= (un);
+			normal[2] /= (un);
+			
+			phongNorm_x[curSpot] = normal[0];
+			phongNorm_y[curSpot] = normal[1];
+			phongNorm_z[curSpot] = normal[2];
+
+			normal[0] = 0;
+			normal[1] = 0;
+			normal[2] = 0;
+		}
+
+		phongNorm_x[(j + 1) * phongVerticalCP - 1] =  0.0f;
+		phongNorm_y[(j + 1) * phongVerticalCP - 1] = -1.0f;
+		phongNorm_z[(j + 1) * phongVerticalCP - 1] =  0.0f;
+	}
+}
+
+
+
 
 
 /**********************************************
--- ass3/subdiv.cpp
+++ ass3/subdiv.cpp
@@ -26,6 +26,7 @@ int H=400;		/* window height */
 int X_OFF = 10;	/* window x offset */
 int Y_OFF = 10;	/* window y offset */
 
+
 /* The dimensions of the viewing frustum */
 GLfloat fleft   = -40.0;
 GLfloat fright  =  40.0;
@@ -41,7 +42,35 @@ void myKeyHandler(unsigned char ch, int x, int y);
 void myMouseButton(int button, int state, int x, int y);
 void endSubdiv(int status);
 
+
+/*From mouse.h*///---------------------------------------
+#define MOUSE_ROTATE_YX		0
+#define MOUSE_ROTATE_YZ		1
+#define MOUSE_ZOOM			2
+
+/* The frustum and zoom factor variables from canvas.c */
+GLfloat zoomFactor = 1.0;
+
+/* The current mode the mouse is in, based on what button(s) is pressed */
+int mouse_mode;
+
+/* The last position of the mouse since the last callback */
+int m_last_x, m_last_y;
+
+/* Function declarations */
+void myMouseButton(int button, int state, int x, int y);
+void myMouseMotion(int x, int y);
+
+//----------------------------------------------------------
+
+
 int main (int argc, char** argv) {
+// Set display modes
+  is3D = false;
+  isWire = false;
+  isPoints = false;
+  isPhong = false;
+
   glutInit(&argc,argv);
   glutInitWindowSize(W, H);
   glutInitWindowPosition(X_OFF, Y_OFF);
@@ -51,6 +80,7 @@ int main (int argc, char** argv) {
   glutDisplayFunc(display);
   glutKeyboardFunc(myKeyHandler);
   glutMouseFunc(myMouseButton);
+  glutMotionFunc(myMouseMotion);
   glutMainLoop();
   return 0;
 }
@@ -60,21 +90,43 @@ void init() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(fleft, fright, fbottom, ftop, -zNear, -zFar);
+	
+	glShadeModel (GL_SMOOTH);
+
+	glEnable(GL_NORMALIZE);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
 }
 
-void display() {
+void display()
+{
 	glEnable(GL_DEPTH_TEST);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+	
+	if (is3D)
+	{
+		glEnable(GL_LIGHTING);
+		glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+		glEnable(GL_LIGHT0);
 
-	/*
-	 * See drawing.c for the definition of these routines.
-	 *
-	 * Note: Only one should be called at a time (based on the
-	 * display mode).
-	 */
-
-	drawSurface();
+		if (isPhong)
+		{
+			drawSurfacePhong();
+		}
+		else
+		{
+			drawSurfaceGouraud();
+		}
+	}
+	else
+	{
+		glDisable(GL_LIGHTING);
+		drawSurface();
+	}
 
     glFlush();  /* Flush all executed OpenGL ops finish */
 
@@ -90,22 +142,245 @@ void myKeyHandler(unsigned char ch, int x, int y) {
 		case 'q':
 			endSubdiv(0);
 			break;
+
+		case 'w':
+			if (originalCP < 5)
+			{
+				printf("Too Few Control Points, Min = 5\n");
+				break;
+			}
+
+			if (!is3D)
+			{
+				generateRotationPoints(3);
+				is3D = true;
+				display();
+			}
+			break;
+
+		case 'z':
+			if (is3D)
+			{
+				is3D = false;
+
+				glMatrixMode(GL_PROJECTION);
+				glLoadIdentity();
+				glOrtho(fleft, fright, fbottom, ftop, -zNear, -zFar);
+
+				display();	
+			}
+			break;
+
+		case 'e':
+			if (is3D)
+			{
+				isWire = !isWire;
+				display();
+			}
+			break;
+
+		case 'r':
+			isPoints = !isPoints;
+			display();
+			break;
+		case 'a':
+			if(is3D && verticalSubLevel < 6)
+			{
+				verticalSubdivide();
+				display();
+			}
+			break;
+
+		case 'b':
+			if(is3D && radialSubLevel < 6)
+			{
+				radialSubdivide();
+				display();
+			}
+			break;
+
+		case 'd':
+			if (is3D)
+			{
+				isPhong = !isPhong;
+				display();
+			}
+			break;
+
+		case 's':
+			if (is3D)
+			{
+				shininess[0] = ((int)shininess[0] + 10) % 128;
+				//printf("shininess = %f\n", shininess[0]);
+				display();
+			}
+			break;
+
+		case 'i':
+			if (is3D && lightPosition[1] > -10.0f)
+			{
+				lightPosition[1] -= 0.5f;
+				//printf("lightPosition = (%g, %g, %g)\n", lightPosition[0], lightPosition[1], lightPosition[2]);
+				display();
+			}
+			break;
+
+		case 'k':
+			if (is3D && lightPosition[1] < 10.0f)
+			{
+				lightPosition[1] += 0.5f;
+				//printf("lightPosition = (%g, %g, %g)\n", lightPosition[0], lightPosition[1], lightPosition[2]);
+				display();
+			}
+			break;
+
+		case 'j':
+			if (is3D && lightPosition[0] > -10.0f)
+			{
+				lightPosition[0] -= 0.5f;
+				//printf("lightPosition = (%g, %g, %g)\n", lightPosition[0], lightPosition[1], lightPosition[2]);
+				display();
+			}
+			break;
+
+		case 'l':
+			if (is3D && lightPosition[0] < 10.0f)
+			{
+				lightPosition[0] += 0.5f;
+				//printf("lightPosition = (%g, %g, %g)\n", lightPosition[0], lightPosition[1], lightPosition[2]);
+				display();
+			}
+			break;
+
 		default:
 			/* Unrecognized keypress */
 		return;
 	}
 
 	glutPostRedisplay();
-
 	return;
 }
 
 void myMouseButton(int button, int state, int x, int y) {
-	if (state == GLUT_DOWN) {
-		if (button == GLUT_LEFT_BUTTON) {
-			// Add a point, if there is room
-			printf("x: %3d, y: %3d\n", x, y);
+	if(!is3D)
+	{
+		if (state == GLUT_DOWN) {
+			if (button == GLUT_LEFT_BUTTON) 
+			{
+				// Add a point, if there is room
+				if(originalCP < 30)
+				{
+					printf("x: %3d, y: %3d Control Point: %d\n", x, y, originalCP);	
+					add2dControlPoint(x,y);
+					display();
+				}
+				else
+				{
+					printf("Exceeded Maximum Control Points of %d\n", MAX_POINT);
+				}
+			}
+			if (button == GLUT_RIGHT_BUTTON)
+			{
+				if(originalCP > 0)
+				{
+					remove2dControlPoint();
+					printf("Deleted Control Point: %d\n" , originalCP);
+					display();
+				}
+				else
+				{
+					printf("No Control Points left to delete.\n");
+				}
+			}
 		}
+	}
+	else // in 3D
+	{
+		if (state == GLUT_DOWN) 
+		{
+			m_last_x = x;
+			m_last_y = y;
+
+			if (button == GLUT_LEFT_BUTTON)
+			{
+				mouse_mode = MOUSE_ROTATE_YX;
+			}
+			else if (button == GLUT_MIDDLE_BUTTON)
+			{
+				mouse_mode = MOUSE_ROTATE_YZ;
+			}
+			else if (button == GLUT_RIGHT_BUTTON)
+			{
+				mouse_mode = MOUSE_ZOOM;
+			}
+		}
+	}
+}
+
+void myMouseMotion(int x, int y) {
+	if(is3D)
+	{
+		double d_x, d_y;	/* The change in x and y since the last callback */
+
+		d_x = x - m_last_x;
+		d_y = y - m_last_y;
+
+		m_last_x = x;
+		m_last_y = y;
+
+		if (mouse_mode == MOUSE_ROTATE_YX) 
+		{
+			/* scaling factors */
+			d_x /= 2.0;
+			d_y /= 2.0;
+
+			glRotatef(d_x, 0.0, 1.0, 0.0);	/* y-axis rotation */
+			glRotatef(-d_y, 1.0, 0.0, 0.0);	/* x-axis rotation */
+
+		} 
+		else if (mouse_mode == MOUSE_ROTATE_YZ) 
+		{
+			/* scaling factors */
+			d_x /= 2.0;
+			d_y /= 2.0;
+
+			glRotatef(d_x, 0.0, 1.0, 0.0);	/* y-axis rotation */
+			glRotatef(-d_y, 0.0, 0.0, 1.0);	/* z-axis rotation */
+
+		} 
+		else if (mouse_mode == MOUSE_ZOOM) 
+		{
+			d_y /= 100.0;
+
+			zoomFactor += d_y;
+
+			if (zoomFactor <= 0.0) 
+			{
+				/* The zoom factor should be positive */
+				zoomFactor = 0.001;
+			}
+
+			printf("zoomFactor: %f\n", zoomFactor);
+			
+			glMatrixMode(GL_PROJECTION);
+			glLoadIdentity();
+
+			/*
+			 * glFrustum must receive positive values for the near and far
+			 * clip planes ( arguments 5 and 6 ).
+			 */
+			glOrtho(fleft*zoomFactor, fright*zoomFactor,
+				fbottom*zoomFactor, ftop*zoomFactor,
+				-zNear, -zFar);
+			/*
+			printf("glFrustum( %f, %f, %f, %f, %f, %f )\n", fleft*zoomFactor, fright*zoomFactor,
+				fbottom*zoomFactor, ftop*zoomFactor,
+				-zNear, -zFar);
+			*/
+		}
+
+		/* Redraw the screen */
+		glutPostRedisplay();
 	}
 }
 
